5df9a21dccf819b41ea344c5822c866a41662c4b
EC2 Default User <ec2-user@ip-172-31-22-146.ec2.internal>
2022-08-02 01:51:55 +0000

    Feat: Ran make Install, make lint, ran docker and kubernetes

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build -t project-four-udacity .
 # Step 2: 
 # List docker images
-
+docker image ls
 # Step 3: 
 # Run flask app
+docker run -d -p 80:80 project-four-udacity
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,18 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath='kel2ck/project-four-udacity'
 
 # Step 2:  
 # Authenticate & tag
+docker login #\
+    #--docker-username=kel2ck \
+    #--docker-password=futocat@1986
+    
+docker image tag project-four-udacity $dockerpath
+
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
